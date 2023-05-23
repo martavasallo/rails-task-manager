53c0623db225f1b44926ca30996c1bc8595b05f1
marta vasallo <mvasalloperez@gmail.com>
2023-05-23 15:59:24 +0100

    Task app

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   root 'tasks#index'
   get '/tasks', to: 'tasks#index'
   get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch '/tasks/:id', to: 'tasks#update'
   get '/tasks/:id', to: 'tasks#show', as: :task
 end
